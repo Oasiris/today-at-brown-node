90fe2c4054a690c0829d28f962c2916d58297126
Oasiris <teracontinens@gmail.com>
2017-12-03 02:34:55 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// =============================================================================
+// Helper Functions
+// =============================================================================
+
+function dateNowReadable() {
+  let myDate = new Date;
+  return myDate.toLocaleString('en-US');
+}
+
+
+// =============================================================================
+// app Initialization
+// =============================================================================
+
+// Sets view (template) engine to pug
+// Modifies the setting of the object `app`
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+// =============================================================================
+// Page Serving
+// =============================================================================
+
+app.get('/', (req, res) => {
+  res.render('sandbox1');
+})
+
+
+
+// =============================================================================
+// App Startup
+// =============================================================================
+
+const server = app.listen(3000, () => {
+  console.log(`[${dateNowReadable()}] Now running on *:3000`);
+});
